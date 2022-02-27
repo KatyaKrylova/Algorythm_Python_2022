749fc10d4b84fee20643dea8372670bd5e21ba2a
KatyaKrylova <4shcool8@mail.ru>
2022-02-27 19:49:17 +0300

    task1

--- src/sorting/bubble_sort.py
+++ src/sorting/bubble_sort.py
@@ -1,36 +1,11 @@
-"""
->>> import io, sys 
->>> sys.stdin = io.StringIO(chr(10).join(['4','4 3 2 1']))  # input
->>> bubble_sort()
-3 4 2 1
-3 2 4 1
-3 2 1 4
-2 3 1 4
-2 1 3 4
-1 2 3 4
-"""
-def bubble_sort():
-    n = int(input())
-    xs = list(map(int, input().split()))
-    swaps = 0
-    for i in range(n - 1):
-        for j in range(n - 1 - i):
-            if xs[j] > xs[j + 1]:
-                xs[j], xs[j + 1] = (xs[j + 1], xs[j])
-                swaps += 1
-                print(*xs)
-    if swaps == 0:
-        print(0)
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod(verbose=True)
-def bubble_sort():
-    n = input()
-    inp_string = input()
-    str_lst = inp_string.split(" ")
-    print(str_lst)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod(verbose=True)
+n=int(input())
+count=0
+mas=list(map(int, (input().split())))#map позволяет применить функцию list для каждого итерируемого объекта
+for i in range(n-1):
+                for j in range(n-i-1):
+                    if mas[j] > mas[j+1]:
+                        mas[j], mas[j+1] = mas[j+1], mas[j]
+                        count+=1
+                        print(" ".join(map(str, mas)))
+if count==0:
+    print(0)
