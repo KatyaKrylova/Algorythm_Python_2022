e8a44f92b45b8dacc0897d0064f7a5f3c55c4a77
KatyaKrylova <4shcool8@mail.ru>
2022-02-27 19:42:40 +0300

    bubblesort

--- src/sorting/bubble_sort.py
+++ src/sorting/bubble_sort.py
@@ -9,7 +9,22 @@
 2 1 3 4
 1 2 3 4
 """
+def bubble_sort():
+    n = int(input())
+    xs = list(map(int, input().split()))
+    swaps = 0
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if xs[j] > xs[j + 1]:
+                xs[j], xs[j + 1] = (xs[j + 1], xs[j])
+                swaps += 1
+                print(*xs)
+    if swaps == 0:
+        print(0)
 
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
 def bubble_sort():
     n = input()
     inp_string = input()
